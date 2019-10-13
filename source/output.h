25149eed3769830a3ccd79a1a84378c407a52499
nalquas <22664233+nalquas@users.noreply.github.com>
2019-10-13 10:31:42 +0200

    Start of code

---
+++ source/output.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define BUFFER_SIZE_X 128
+#define BUFFER_SIZE_Y 32
+
+// Screen buffer
+char buffer[BUFFER_SIZE_X][BUFFER_SIZE_Y] = {};
+
+// Replace the entire buffer with a given char
+void clear(char c) {
+    for (int y = 0; y < BUFFER_SIZE_Y; y++) {
+        for (int x = 0; x < BUFFER_SIZE_X; x++) {
+            buffer[x][y] = c;
+        }
+    }
+}
+
+// Method to output the buffer to terminal by printing it
+void printBuffer() {
+    for (int y = 0; y < BUFFER_SIZE_Y; y++) {
+        for (int x = 0; x < BUFFER_SIZE_X; x++) {
+            printf("%c", buffer[x][y]);
+        }
+        printf("\n");
+    }
+}
---
+++ source/snake.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "output.h"
+
+int main(int argc, char const *argv[]) {
+    clear(' ');
+    printBuffer();
+    return 0;
+}
